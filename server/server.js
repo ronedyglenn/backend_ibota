a9369b311e82c9a3a5abebad9dea4a0389b26de1
ronedyglenn <ronedyglenn@gmail.com>
2024-01-18 05:55:34 +0100

    configuration Swagger

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const cors = require("cors")
 const app = express();
 const connectDB = require('../config/db')
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express')
 // Définition du port d'écoute, utilisant le port spécifié dans les variables d'environnement (process.env.PORT) ou le port 3000 par défaut
-
 const port =process.env.PORT;
 
 app.use(express.urlencoded({extended: true}))
@@ -15,10 +16,6 @@ app.use(cors())
 
 connectDB();
 
-
-
-
-
 // Middleware pour traiter les req...
 app.use(express.json());
 
@@ -26,7 +23,28 @@ app.use(express.json());
 // routes
 app.use('/', require("../routes/routes.js"));
 
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API Utilisateur',
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: 'https://ibota.onrender.com/',
+        },
+      ],
+    },
+    apis: ['../routes/*js'], // Spécifiez vos fichiers d'API ici
+  };
 
+const documentation = swaggerJsDoc(options)
+app.use(
+    "/api-doc",
+    swaggerUi.setup(documentation),
+    swaggerUi.serve
+)
 
 // Lancement du serveur, écoutant les connexions sur le port spécifié
 app.listen(port, () => console.log('Le serveur a démarré au port ' + port));
