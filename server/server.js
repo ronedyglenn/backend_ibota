e12380c85179d61755eecf321f1fdfd6ff9fbc4e
ronedyglenn <ronedyglenn@gmail.com>
2024-01-18 07:35:56 +0100

    mise à jour de la docummentation swagger

--- server/server.js
+++ server/server.js
@@ -23,28 +23,58 @@ app.use(express.json());
 // routes
 app.use('/', require("../routes/routes.js"));
 
+
+
 const options = {
     definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'API Utilisateur',
-        version: '1.0.0',
-      },
-      servers: [
-        {
-          url: 'https://ibota.onrender.com',
+        openapi: '3.0.0',
+        info: {
+            title: 'API Utilisateur',
+            version: '1.0.0',
+            description: "Il s'agit de l'api qui permettra de récupérer les données sur les naissances et les utilisateurs de ibota"
         },
-      ],
+        servers: [
+            {
+                url: 'https://ibota.onrender.com',
+            },
+        ],
+        paths: {
+            "/api/utilisateur": {
+              get: {
+                summary: "Récupérer la liste des utilisateurs",
+                description: "Endpoint pour obtenir la liste des utilisateurs.",
+                responses: {
+                  200: {
+                    description: "Réponse réussie",
+                    content: {
+                      "application/json": {
+                        example: {
+                          "utilisateurs": [
+                            {
+                              "message": "Hello"
+                            },
+                            
+                          ]
+                        }
+                      }
+                    }
+                  },
+                },
+            }
+        },
+
+        
     },
-    apis: ['../routes/*js'], // Spécifiez vos fichiers d'API ici
-  };
+},
+apis: ['../routes/*.js'], // Spécifiez vos fichiers d'API ici
+}
 
-const documentation = swaggerJsDoc(options)
+const spacs = swaggerJsDoc(options)
+
 app.use(
-    "/api-doc",
-    swaggerUi.setup(documentation),
-    swaggerUi.serve
-)
-
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(spacs)
+    )
 // Lancement du serveur, écoutant les connexions sur le port spécifié
 app.listen(port, () => console.log('Le serveur a démarré au port ' + port));
